09bc6bf43322376d39151d00a39b5c0833356c69
anaisjulieta <anaisrocha1@gmail.com>
2023-07-11 21:42:50 -0400

    agregué la seguridad, creé el rol Admin y sume rutas protegidas

---
+++ src/main/java/Controllers/WebController.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WebController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String mostrarLogin() {
+        return "login";//Retornando la vista login.html
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String mostrarHello() {
+        return "hello";
+    }
+
+}
